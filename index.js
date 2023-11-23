02574aef46309df2ec31c4197131fde4cd56d0f1
blessing <bolaolaniyi69@gmail.com>
2023-11-23 13:44:18 +0100

    done

---
+++ index.js
@@ -0,0 +1,196 @@
+let allCommerce=[
+    {
+        "id": 1,
+        "image": "https://cdn.pixabay.com/photo/2014/09/27/00/48/rolex-463010_640.jpg",
+        "title": "rolex wrist watch",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price":"$109.95",
+        },
+    {
+        "id": 2,
+        "image": "https://cdn.pixabay.com/photo/2013/07/11/15/30/male-watch-144648_1280.jpg",
+        "title": "Qmax swiss watch",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$87.95",
+        },
+    {
+        "id": 3,
+        "image": "https://cdn.pixabay.com/photo/2013/09/30/21/58/male-watch-188782_1280.jpg",
+        "title": "Hublot wrist",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price":"$155.95",
+        },
+    {
+        "id": 4,
+        "image": "https://cdn.pixabay.com/photo/2016/04/06/14/04/watch-1311821_1280.jpg",
+        "title": "Ladys wrist watch",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price":"$100",
+        },
+    {
+        "id": 5,
+        "image": "https://cdn.pixabay.com/photo/2020/04/08/16/05/stylish-5017916_640.jpg",
+        "title": "Stylish mens leather jacket",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$60.95",
+        },
+    {
+        "id": 6,
+        "image": "https://cdn.pixabay.com/photo/2023/09/13/21/16/footwear-8251722_640.jpg",
+        "title": "Pink jean jacket for men ",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$40.95",
+        },
+    {
+        "id": 7,
+        "image": "https://media.istockphoto.com/id/1084128060/photo/blank-black-mens-pants-mock-up-isolated.jpg?s=612x612&w=0&k=20&c=0GXI1ysbq_YTIUxVo_baTHKNywT3n3-nX-TXi-hFKnk=",
+        "title": "Black jean for men",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$109.95",
+        },
+    {
+        "id": 8,
+        "image": "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.jpg?s=612x612&w=0&k=20&c=fd6mBZ93xbu1rkGys5u6YD8_irIs5Xzo80QWVOooxV0=",
+        "title": "Mens blazer",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$109.95",
+        },
+    {
+        "id": 9,
+        "image": "https://cdn.pixabay.com/photo/2016/03/04/20/12/heels-1236641_640.jpg",
+        "title": "Bride white heels",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$175",
+        },
+    {
+        "id": 10,
+        "image": "https://cdn.pixabay.com/photo/2016/03/27/22/16/fashion-1284496_640.jpg",
+        "title": "Red fashion heels",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$154",
+        },
+    {
+        "id": 11,
+        "image": "https://cdn.pixabay.com/photo/2015/01/06/16/24/shoe-590516_640.jpg",
+        "title": "Blue shoe heels",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$99.05",
+        },
+    {
+        "id": 12,
+        "image": "https://cdn.pixabay.com/photo/2017/02/21/15/08/high-heels-2086307_640.jpg",
+        "title": "Espresso red heels",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$199.99",
+        },
+    {
+        "id": 13,
+        "image": "https://cdn.pixabay.com/photo/2017/01/20/15/06/oranges-1995056_640.jpg",
+        "title": "Citrus sinensis",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$10",
+        },
+    {
+        "id": 14,
+        "image": "https://cdn.pixabay.com/photo/2017/11/18/17/09/strawberries-2960533_1280.jpg",
+        "title": "Fragaria ananassa",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$50.60",
+        },
+    {
+        "id": 15,
+        "image": "https://cdn.pixabay.com/photo/2021/07/19/16/04/pizza-6478478_1280.jpg",
+        "title": "Domino pizza",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$150",
+        },
+    {
+        "id": 16,
+        "image": "https://cdn.pixabay.com/photo/2013/08/11/19/46/coffee-171653_1280.jpg",
+        "title": "Coffe bean black",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$75",
+        },
+    {
+        "id": 17,
+        "image": "https://cdn.pixabay.com/photo/2017/01/09/02/02/pink-wine-1964457_640.jpg",
+        "title": "Pink wine champgne",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$185.70",
+        },
+    {
+        "id": 18,
+        "image": "https://cdn.pixabay.com/photo/2017/06/26/12/49/red-wine-2443699_640.jpg",
+        "title": "Allessikwes red wine",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$109",
+        },
+    {
+        "id": 19,
+        "image": "https://cdn.pixabay.com/photo/2018/12/28/01/34/rum-3898745_1280.jpg",
+        "title": "Capt jack rum drink",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$198",
+        },
+    {
+        "id": 20,
+        "image": "https://cdn.pixabay.com/photo/2017/03/17/16/54/irish-whiskey-2152126_1280.jpg",
+        "title": "Tullamore irish whiskey",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": "$140",
+        },
+    ] ;
+    
+    let allCom = document.getElementById("commerce");
+console.log(allCom);
+
+let createProduct = () => {
+    allCommerce.map((value, index) => {
+      console.log(index);
+      const card = `
+          <div key=${index} class='card'>
+              <img src=${value.image} alt=${value.title} class=''/>
+              <h1>${value.title}</h1>
+               <p class="desc">${value.description.substr(0, 10)}...</p>
+              <small class="price">${value.price}</small>
+              <del class="del">$200</del><br>
+              <button class="btn">Shop Now</button>
+             
+          </div>
+    `;
+      allCom.innerHTML += card;
+    });
+  };
+  createProduct();
+  
+//   const button= document.getElementById("button");
+//   const bag = document.getElementById("new-product");
+//   const createProduct2 = () => {
+//     bag.map((commerce) => {
+//       const newCard = document.createElement("div");
+//       newProducts.appendChild(newCard);
+//       const image = document.createElement("img");
+//       image.src = commerce.image;
+//       const title = document.createElement("h2");
+//       title.innerHTML = commerce.title;
+//       const description = document.createElement("p");
+//       description.innerHTML = commerce.desc;
+//       const price = document.createElement("p");
+//       price.innerHTML = commerce.price;
+//       // const play = document.createElement("button");
+//       // play.innerHTML = "Play";
+  
+//       product.appendChild(image);
+//       product.appendChild(title);
+//       product.appendChild(description);
+//       product.appendChild(price);
+//     //   newCard.appendChild(play);
+  
+//       newProducts.className = "card";
+  
+//       newProducts.appendChild(newCard);
+//     });
+//   };
+  
+//   createProduct2();
+    
